dd9677dd20430b22daf2f577abc35f38f1ab54d1
amr-radwan1 <amrradwan2006@gmail.com>
2025-11-12 22:08:19 -0500

    added enum for activation func

--- mlp.cpp
+++ mlp.cpp
@@ -3,12 +3,19 @@
 #include <ctime>
 #include <cmath> 
 
+enum ActivationFunction {
+    SIGMOID,
+    RELU,
+    SOFTMAX
+};
+
 struct Neuron {
     float value; // output after activation
     float bias;
     float* weights;
     
-    float delta;       
+    float delta;
+    float preActivation; // store z before activation for derivatives
 };
 
 struct Layer {
@@ -17,6 +24,8 @@ struct Layer {
     
     float* layerInputs; // store inputs to layer for derivative in backprop
     int inputSize;
+    
+    ActivationFunction activation;
 };
 
 struct MLP {
@@ -24,16 +33,20 @@ struct MLP {
     int layerCount;
 };
 
+
+
+
 float randomWeight() {
     return ((float) rand() / RAND_MAX) * 2 - 1;
 }
 
-Layer createLayer(int neuronCount, int inputPerNeuron) {
+Layer createLayer(int neuronCount, int inputPerNeuron, ActivationFunction activation = SIGMOID) {
     Layer layer;
     layer.neuronCount = neuronCount;
     layer.neurons = new Neuron[neuronCount];
     layer.inputSize = inputPerNeuron;
-    layer.layerInputs = new float[inputPerNeuron];  
+    layer.layerInputs = new float[inputPerNeuron];
+    layer.activation = activation;
 
     for (int i = 0; i < neuronCount; i++) {
         layer.neurons[i].bias = randomWeight();
@@ -57,7 +70,32 @@ float dotProduct(float* a, float* b, int size) {
 }
 
 float sigmoid(float x) {
-    return 1 / (1 + expf(-x));
+    return 1.0f / (1.0f + expf(-x));
+}
+
+float relu(float x) {
+    return (x > 0.0f) ? x : 0.0f;
+}
+
+float reluDerivative(float x) {
+    return (x > 0.0f) ? 1.0f : 0.0f;
+}
+
+void softmax(float* input, int size, float* output) {
+    float maxVal = input[0];
+    for (int i = 1; i < size; i++) {
+        if (input[i] > maxVal) maxVal = input[i];
+    }
+    
+    float sum = 0.0f;
+    for (int i = 0; i < size; i++) {
+        output[i] = expf(input[i] - maxVal);
+        sum += output[i];
+    }
+    
+    for (int i = 0; i < size; i++) {
+        output[i] /= sum;
+    }
 }
 
 
@@ -69,19 +107,34 @@ void forwardLayer(Layer& layer, float* inputs, int inputSize, float* outputs) {
     
     for (int i = 0; i < layer.neuronCount; i++) {
         float activation = dotProduct(layer.neurons[i].weights, inputs, inputSize) + layer.neurons[i].bias;
-        layer.neurons[i].value = sigmoid(activation);
+        layer.neurons[i].preActivation = activation;
+        if (layer.activation == RELU) {
+            layer.neurons[i].value = relu(activation);
+        } else if (layer.activation == SOFTMAX) {
+            layer.neurons[i].value = activation;
+        } else {
+            layer.neurons[i].value = sigmoid(activation);
+        }
         outputs[i] = layer.neurons[i].value;
+    }
+    
+    if (layer.activation == SOFTMAX) {
+        softmax(outputs, layer.neuronCount, outputs);
+        for (int i = 0; i < layer.neuronCount; i++) {
+            layer.neurons[i].value = outputs[i];
+        }
     }
 }
 
-MLP createMLP(int* layerSizes, int layerCount, int inputSize) {
+MLP createMLP(int* layerSizes, int layerCount, int inputSize, ActivationFunction* activations = nullptr) {
     MLP mlp;
     mlp.layerCount = layerCount;
     mlp.layers = new Layer[layerCount];
 
     for (int i = 0; i < layerCount; i++) {
         int prevLayerSize = (i == 0) ? inputSize : layerSizes[i - 1];
-        mlp.layers[i] = createLayer(layerSizes[i], prevLayerSize);
+        ActivationFunction act = activations ? activations[i] : SIGMOID;
+        mlp.layers[i] = createLayer(layerSizes[i], prevLayerSize, act);
     }
 
     return mlp;
@@ -124,8 +177,14 @@ void backwardMLP(MLP& mlp, float* input, int inputSize, float* predictions, floa
     for (int i = 0; i < outputLayer.neuronCount; i++) {
         float output = outputLayer.neurons[i].value;
         float error = output - targets[i]; // dL / d y_pred
-        float sigmoidDerivative = output * (1.0f - output); // dy_pred / d output
-        outputLayer.neurons[i].delta = error * sigmoidDerivative; // dL / d output
+        if (outputLayer.activation == SOFTMAX) {
+            outputLayer.neurons[i].delta = error; // dL / d output
+        } else if (outputLayer.activation == RELU) {
+            outputLayer.neurons[i].delta = error * reluDerivative(outputLayer.neurons[i].preActivation);
+        } else {
+            float sigmoidDerivative = output * (1.0f - output); // dy_pred / d output
+            outputLayer.neurons[i].delta = error * sigmoidDerivative; // dL / d output
+        }
 
         for (int j = 0; j < outputLayer.inputSize; j++) {
             // dL / d output * d output / d weight
@@ -147,8 +206,12 @@ void backwardMLP(MLP& mlp, float* input, int inputSize, float* predictions, floa
                 sumDeltas += nextLayer.neurons[k].weights[i] * nextLayer.neurons[k].delta;
             }
             float output = layer.neurons[i].value;
-            float sigmoidDerivative = output * (1.0f - output);
-            layer.neurons[i].delta = sumDeltas * sigmoidDerivative;
+            if (layer.activation == RELU) {
+                layer.neurons[i].delta = sumDeltas * reluDerivative(layer.neurons[i].preActivation);
+            } else {
+                float sigmoidDerivative = output * (1.0f - output);
+                layer.neurons[i].delta = sumDeltas * sigmoidDerivative;
+            }
 
             for (int j = 0; j < layer.inputSize; j++) {
                 float gradient = layer.neurons[i].delta * layer.layerInputs[j];
@@ -183,14 +246,15 @@ int main() {
     };
     
     int layerSizes[] = {8, 8, 2}; 
+    ActivationFunction activations[] = {RELU, RELU, SOFTMAX};
     int layerCount = 3;
     int inputSize = 2;
     
-    MLP mlp = createMLP(layerSizes, layerCount, inputSize);
+    MLP mlp = createMLP(layerSizes, layerCount, inputSize, activations);
     
-    float learningRate = 0.1f;
+    float learningRate = 0.01f;
 
-    for (int epoch = 0; epoch < 10000; epoch++) {
+    for (int epoch = 0; epoch < 500; epoch++) {
         for (int i = 0; i < 4; i++) {
             float output[2];
             forwardMLP(mlp, xorInputs[i], inputSize, output);
